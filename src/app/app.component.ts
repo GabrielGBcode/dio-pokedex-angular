8e5bd9c44c5c42e117fb684d982a9e41c9552491
Gabriel GB <105217214+GabrielGBcode@users.noreply.github.com>
2023-10-04 18:48:48 -0300

    created pokedex search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>Prcure o pokémon</h1>
+    <app-card></app-card>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'services-angular';
